ac3b21de1f009fd37c022269d0b9ce776912b52d
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2025-01-11 03:59:55 +0530

    feat: Add loading state with Loader component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
-import Image from "next/image";
+"use client"
 
+import { useState, useEffect } from "react";
+import Image from "next/image";
 import ComingSoon from "@/components/comingSoon/ComingSoon";
 import Faq from "@/components/FAQ/Faq";
 
 import bgImg from "../../public/Images/backNU.avif";
 import ParallaxScene from "@/components/Herosection/HeroSection";
+import Loader from "@/components/loader/loader";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading state for 3 seconds, adjust as needed
+    const timer = setTimeout(() => setIsLoading(false), 500); // You can adjust this timeout
+    return () => clearTimeout(timer); // Cleanup on component unmount
+  }, []);
+
   return (
     <>
-      {/* Background */}
-      
-      <ParallaxScene />
-      <Faq />
+      {isLoading ? (
+        <Loader /> 
+      ) : (
+        <>
+          <ParallaxScene />
+          <Faq />
+        </>
+      )}
     </>
   );
 }
