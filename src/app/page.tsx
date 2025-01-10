f3802bb9e19070a212113b7836a21e40312314ae
Debajyoti <chatterjeedebajyoti254@gmail.com>
2025-01-11 03:22:17 +0530

    Minor Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,30 +13,6 @@ import PastSponsors from "@/components/PastSponsor/PastSponsors";
 export default function Home() {
   return (
     <>
-      {/* Background */}
-      {/* <div className="fixed inset-0 -z-30"> */}
-      {/* Background Image */}
-      {/* <Image
-          src={bgImg}
-          fill={true}
-          alt="Background"
-          className="object-cover"
-        /> */}
-
-      {/* Logo Image */}
-      {/* <Image
-          src="/Images/logo.png"
-          width={600}
-          height={600}
-          alt="Logo"
-          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mix-blend-soft-light"
-        /> */}
-
-      {/* Dark Overlay */}
-      {/* <div className="w-full h-full absolute top-0 bg-[#00000085]" />
-      </div> */}
-
-      {/* Content */}
       <AboutUs />
       <PastSponsors />
     </>
