475a733782591b9ec2c4cb3b0e0c81a1a7f385a0
Ayush <135319056+ayussh-2@users.noreply.github.com>
2025-01-11 04:10:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
-
+import Image from "next/image";
+import Faq from "@/components/FAQ/Faq";
+import ParallaxScene from "@/components/Herosection/HeroSection";
+import AboutUs from "@/components/About Us/AboutUs";
+import PastSponsors from "@/components/PastSponsor/PastSponsors";
 import Events from "@/components/Events/Events"
 
 export default function Home() {
   return (
     <>
+      <ParallaxScene />
+      <AboutUs />
       <Events />
+      <PastSponsors />
+      <Faq />
     </>
   );
 }
